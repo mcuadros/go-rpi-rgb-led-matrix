2843f6de381ed140ca8ad04f79e4dc6d4e58e5cb
Mike Ryan <falter@gmail.com>
2018-03-05 11:28:39 -0600

    Add cli options to examples

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -9,18 +9,28 @@ import (
 )
 
 var (
-	rows       = flag.Int("led-rows", 32, "number of rows supported")
-	parallel   = flag.Int("led-parallel", 1, "number of daisy-chained panels")
-	chain      = flag.Int("led-chain", 2, "number of displays daisy-chained")
-	brightness = flag.Int("brightness", 100, "brightness (0-100)")
+	rows                     = flag.Int("led-rows", 32, "number of rows supported")
+	cols                     = flag.Int("led-cols", 32, "number of columns supported")
+	parallel                 = flag.Int("led-parallel", 1, "number of daisy-chained panels")
+	chain                    = flag.Int("led-chain", 2, "number of displays daisy-chained")
+	brightness               = flag.Int("brightness", 100, "brightness (0-100)")
+	hardware_mapping         = flag.String("led-gpio-mapping", "regular", "Name of GPIO mapping used.")
+	show_refresh             = flag.Bool("led-show-refresh", false, "Show refresh rate.")
+	inverse_colors           = flag.Bool("led-inverse", false, "Switch if your matrix has inverse colors on.")
+	disable_hardware_pulsing = flag.Bool("led-no-hardware-pulse", false, "Don't use hardware pin-pulse generation.")
 )
 
 func main() {
 	config := &rgbmatrix.DefaultConfig
 	config.Rows = *rows
+	config.Cols = *cols
 	config.Parallel = *parallel
 	config.ChainLength = *chain
 	config.Brightness = *brightness
+	config.HardwareMapping = *hardware_mapping
+	config.ShowRefreshRate = *show_refresh
+	config.InverseColors = *inverse_colors
+	config.DisableHardwarePulsing = *disable_hardware_pulsing
 
 	m, err := rgbmatrix.NewRGBLedMatrix(config)
 	fatal(err)
--- examples/image/main.go
+++ examples/image/main.go
@@ -10,11 +10,16 @@ import (
 )
 
 var (
-	rows       = flag.Int("led-rows", 32, "number of rows supported")
-	parallel   = flag.Int("led-parallel", 1, "number of daisy-chained panels")
-	chain      = flag.Int("led-chain", 2, "number of displays daisy-chained")
-	brightness = flag.Int("brightness", 100, "brightness (0-100)")
-	img        = flag.String("image", "", "image path")
+	rows                     = flag.Int("led-rows", 32, "number of rows supported")
+	cols                     = flag.Int("led-cols", 32, "number of columns supported")
+	parallel                 = flag.Int("led-parallel", 1, "number of daisy-chained panels")
+	chain                    = flag.Int("led-chain", 2, "number of displays daisy-chained")
+	brightness               = flag.Int("brightness", 100, "brightness (0-100)")
+	hardware_mapping         = flag.String("led-gpio-mapping", "regular", "Name of GPIO mapping used.")
+	show_refresh             = flag.Bool("led-show-refresh", false, "Show refresh rate.")
+	inverse_colors           = flag.Bool("led-inverse", false, "Switch if your matrix has inverse colors on.")
+	disable_hardware_pulsing = flag.Bool("led-no-hardware-pulse", false, "Don't use hardware pin-pulse generation.")
+	img                      = flag.String("image", "", "image path")
 
 	rotate = flag.Int("rotate", 0, "rotate angle, 90, 180, 270")
 )
@@ -25,9 +30,14 @@ func main() {
 
 	config := &rgbmatrix.DefaultConfig
 	config.Rows = *rows
+	config.Cols = *cols
 	config.Parallel = *parallel
 	config.ChainLength = *chain
 	config.Brightness = *brightness
+	config.HardwareMapping = *hardware_mapping
+	config.ShowRefreshRate = *show_refresh
+	config.InverseColors = *inverse_colors
+	config.DisableHardwarePulsing = *disable_hardware_pulsing
 
 	m, err := rgbmatrix.NewRGBLedMatrix(config)
 	fatal(err)
